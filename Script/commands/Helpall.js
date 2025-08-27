30ef6330838cff0ea0f41ab4614477334b7b2e2b
RA NA <btor4085@gmail.com>
2025-08-27 21:48:36 +0400

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -29,17 +29,17 @@ module.exports.run = async function ({ api, event }) {
  const finalText = `╔═══❖ 🌟 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓 🌟 ❖═══╗
 ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 ╠═════🔰 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🔰═════╣
-║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐑𝐀𝐍𝐀 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐑𝐀𝐍𝐀
+║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐑𝐎𝐍𝐈 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐑𝐎𝐍𝐈
 ║ 📦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬: ${allCommands.length} 
 ╚═══════════════════════╝`;
 
  
  const backgrounds = [
- "https://i.imgur.com/DqbvprE.jpeg",
- "https://i.imgur.com/DqbvprE.jpeg",
- "https://i.imgur.com/DqbvprE.jpeg",
- "https://i.imgur.com/DqbvprE.jpeg"
+ "https://i.imgur.com/AQJyJoM.jpeg",
+ "https://i.imgur.com/AQJyJoM.jpeg",
+ "https://i.imgur.com/AQJyJoM.jpeg",
+ "https://i.imgur.com/AQJyJoM.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
